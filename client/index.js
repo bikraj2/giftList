159dd1c8fecc523e1547826286ce28b9255505fc
bikraj2 <bikrajshrestha2@gmail.com>
2023-06-03 18:56:16 +0545

    Done

---
+++ client/index.js
@@ -0,0 +1,21 @@
+const axios = require('axios');
+const niceList = require('../utils/niceList.json');
+const MerkleTree1 = require('./MerkleTree');
+const MerkleTree2 = require('../utils/MerkleTree');
+const { text } = require('express');
+
+const serverUrl = 'http://localhost:1225';
+
+async function main() {
+ const merkleTree = new MerkleTree1(niceList)
+ const toCheck = 'Norman Block';
+ const proof = merkleTree.getProof(toCheck);
+  const { data: gift } = await axios.post(`${serverUrl}/gift`, {
+   proof:proof,
+   data:toCheck
+  });
+  console.log({ gift });
+}
+
+
+main();
